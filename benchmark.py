Fix units of batch_size_mb

benchmark.py
@@ -60,7 +60,7 @@ def main():
     target_shape = [1, patch_size, patch_size, 1]
     loss = quantile_score
     num_outputs = 3
-    batch_size_mb = 4 * np.product(pred_shape) * args.batch_size
+    batch_size_mb = 4 * np.product(pred_shape) * args.batch_size / 1024 / 1024
 
     steps_per_epoch = int(args.dataset_size / 4 / np.product(pred_shape) / args.batch_size)
     print(steps_per_epoch)